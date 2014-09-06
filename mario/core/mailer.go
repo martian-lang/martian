4193431bd886711ea0333c22bac2fc8f981a0def
Alex Wong <alex@10xtechnologies.com>
2014-09-05 22:59:28 -0700

    MARSOC-27 #resolve
    MARSOC-30 #resolve

--- mario/core/mailer.go
+++ mario/core/mailer.go
@@ -15,21 +15,17 @@ import (
 
 type Mailer struct {
 	InstanceName string
-	username     string
-	password     string
 	host         string
 	port         int
 	notifyEmail  string
 	debug        bool
 }
 
-func NewMailer(instanceName string, username string, password string, notifyEmail string, debug bool) *Mailer {
+func NewMailer(instanceName string, smtphost string, notifyEmail string, debug bool) *Mailer {
 	self := &Mailer{}
 	self.InstanceName = strings.ToLower(instanceName)
-	self.username = username
-	self.password = password
-	self.host = "smtp.gmail.com"
-	self.port = 587
+	self.host = smtphost
+	self.port = 25
 	self.notifyEmail = notifyEmail
 	self.debug = debug
 	return self
@@ -71,7 +67,7 @@ func (self *Mailer) Sendmail(to []string, subject string, body string) error {
 
 	// Build template context.
 	context := &SmtpTemplateData{
-		fmt.Sprintf("Mario Lopez <%s>", self.username),
+		fmt.Sprintf("Mario Lopez <%s>", self.notifyEmail),
 		self.notifyEmail,
 		subject,
 		body,
@@ -84,11 +80,10 @@ func (self *Mailer) Sendmail(to []string, subject string, body string) error {
 	_ = t.Execute(&doc, context)
 
 	// Send email.
-	auth := smtp.PlainAuth("", self.username, self.password, self.host)
 	return smtp.SendMail(
 		fmt.Sprintf("%s:%d", self.host, self.port),
-		auth,
-		self.username,
+		nil,
+		self.notifyEmail,
 		recipients,
 		doc.Bytes(),
 	)
