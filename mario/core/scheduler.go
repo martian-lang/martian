b39d4b18a80e76b16a14a474fb736326a668ab4d
Alex Wong <alex@10xtechnologies.com>
2014-09-14 14:35:11 -0700

    add switch for scheduler logging
    - cosmetic cleanups
    
    
    Former-commit-id: a841057aed8588998b6dea93f26a12ab4a00d132

--- mario/core/scheduler.go
+++ mario/core/scheduler.go
@@ -80,41 +80,57 @@ func (self *Scheduler) Enqueue(cmd *exec.Cmd, threads int, memGB int,
 	stdoutFile *os.File, stderrFile *os.File) {
 
 	go func() {
+		log := false
 		defer stdoutFile.Close()
 		defer stderrFile.Close()
 		if threads > self.cores {
-			LogInfo("schedlr", "Need %d core%s but settling for %d.", threads,
-				pluralize(threads), self.cores)
+			if log {
+				LogInfo("schedlr", "Need %d core%s but settling for %d.", threads,
+					pluralize(threads), self.cores)
+			}
 			threads = self.cores
 		}
 		if memGB > self.memGB {
-			LogInfo("schedlr", "Need %d GB but settling for %d.", memGB,
-				self.memGB)
+			if log {
+				LogInfo("schedlr", "Need %d GB but settling for %d.", memGB,
+					self.memGB)
+			}
 			memGB = self.memGB
 		}
 
 		// Acquire cores.
-		LogInfo("schedlr", "Waiting for %d core%s.", threads, pluralize(threads))
+		if log {
+			LogInfo("schedlr", "Waiting for %d core%s.", threads, pluralize(threads))
+		}
 		self.coreSem.P(threads)
-		LogInfo("schedlr", "Acquiring %d core%s (%d in use).", threads,
-			pluralize(threads), len(self.coreSem))
+		if log {
+			LogInfo("schedlr", "Acquiring %d core%s (%d in use).", threads,
+				pluralize(threads), len(self.coreSem))
+		}
 
 		// Acquire memory.
-		LogInfo("schedlr", "Waiting for %d GB.", memGB)
+		if log {
+			LogInfo("schedlr", "Waiting for %d GB.", memGB)
+		}
 		self.memGBSem.P(memGB)
-		LogInfo("schedlr", "Acquiring %d GB (%d in use).", memGB,
-			len(self.memGBSem))
+		if log {
+			LogInfo("schedlr", "Acquiring %d GB (%d in use).", memGB,
+				len(self.memGBSem))
+		}
 
 		cmd.Start()
 		cmd.Wait()
 
 		self.coreSem.V(threads)
-		LogInfo("schedlr", "Releasing %d core%s (%d in use).", threads,
-			pluralize(threads), len(self.coreSem))
-
+		if log {
+			LogInfo("schedlr", "Releasing %d core%s (%d in use).", threads,
+				pluralize(threads), len(self.coreSem))
+		}
 		self.memGBSem.V(memGB)
-		LogInfo("schedlr", "Releasing %d GB (%d in use).", memGB,
-			len(self.memGBSem))
+		if log {
+			LogInfo("schedlr", "Releasing %d GB (%d in use).", memGB,
+				len(self.memGBSem))
+		}
 	}()
 }
 
