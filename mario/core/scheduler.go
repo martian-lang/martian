362072f7951d40ac0c47c0b01a9a6f664dc0a788
Alex Wong <alex@10xtechnologies.com>
2014-09-17 13:03:47 -0700

    restructure resource management code
    - fix bug capping SGE jobs to 1GB
    - overall simplify and refactor
    - unify capping and filtering
    - move default values closer to point of use
    - clarify variable naming and comment documentation

--- mario/core/scheduler.go
+++ mario/core/scheduler.go
@@ -27,8 +27,8 @@ func (s semaphore) V(n int) {
 }
 
 type Scheduler struct {
-	cores    int
-	memGB    int
+	maxCores int
+	maxMemGB int
 	coreSem  semaphore
 	memGBSem semaphore
 	queue    []*exec.Cmd
@@ -41,37 +41,46 @@ func pluralize(n int) string {
 	return "s"
 }
 
-func NewScheduler(reqCores int, reqMem int) *Scheduler {
-	MAXMEM_FRACTION := 0.75
-
+func NewScheduler(userMaxCores int, userMaxMemGB int) *Scheduler {
 	self := &Scheduler{}
 
-	self.cores = runtime.NumCPU()
-	if reqCores > 0 {
-		self.cores = reqCores
+	// Set max number of cores usable at one time.
+	if userMaxCores > 0 {
+		// If user specified --maxcores, use that value for max usable cores.
+		self.maxCores = userMaxCores
 		LogInfo("schedlr", "Using %d core%s, per --maxcores option.",
-			self.cores, pluralize(self.cores))
+			self.maxCores, pluralize(self.maxCores))
 	} else {
+		// Otherwise, set max usable cores to total number of cores reported
+		// by the system.
+		self.maxCores = runtime.NumCPU()
 		LogInfo("schedlr", "Using %d core%s available on system.",
-			self.cores, pluralize(self.cores))
+			self.maxCores, pluralize(self.maxCores))
 	}
 
-	mem := sigar.Mem{}
-	mem.Get()
-	self.memGB = int(float64(mem.Total) * MAXMEM_FRACTION / 1000000000)
-	if self.memGB < 1 {
-		self.memGB = 1
-	}
-	if reqMem > 0 {
-		self.memGB = reqMem
-		LogInfo("schedlr", "Using %d GB, per --maxmem option.", self.memGB)
+	// Set max GB of memory usable at one time.
+	if userMaxMemGB > 0 {
+		// If user specified --maxmem, use that value for max usable GB.
+		self.maxMemGB = userMaxMemGB
+		LogInfo("schedlr", "Using %d GB, per --maxmem option.", self.maxMemGB)
 	} else {
-		LogInfo("schedlr", "Using %d GB, %d%% of system memory.", self.memGB,
+		// Otherwise, set max usable GB to MAXMEM_FRACTION * GB of total
+		// memory reported by the system.
+		MAXMEM_FRACTION := 0.75
+		sysMem := sigar.Mem{}
+		sysMem.Get()
+		sysMemGB := int(float64(sysMem.Total) * MAXMEM_FRACTION / 1073741824)
+		// Set floor to 1GB.
+		if sysMemGB < 1 {
+			sysMemGB = 1
+		}
+		self.maxMemGB = sysMemGB
+		LogInfo("schedlr", "Using %d GB, %d%% of system memory.", self.maxMemGB,
 			int(MAXMEM_FRACTION*100))
 	}
 
-	self.coreSem = make(semaphore, self.cores)
-	self.memGBSem = make(semaphore, self.memGB)
+	self.coreSem = make(semaphore, self.maxCores)
+	self.memGBSem = make(semaphore, self.maxMemGB)
 	self.queue = []*exec.Cmd{}
 	return self
 }
@@ -80,22 +89,33 @@ func (self *Scheduler) Enqueue(cmd *exec.Cmd, threads int, memGB int,
 	stdoutFile *os.File, stderrFile *os.File) {
 
 	go func() {
-		log := false
+		log := true // convenience flag for toggling debug logging
+
 		defer stdoutFile.Close()
 		defer stderrFile.Close()
-		if threads > self.cores {
+
+		// Sanity check and cap to self.maxCores.
+		if threads < 1 {
+			threads = 1
+		}
+		if threads > self.maxCores {
 			if log {
 				LogInfo("schedlr", "Need %d core%s but settling for %d.", threads,
-					pluralize(threads), self.cores)
+					pluralize(threads), self.maxCores)
 			}
-			threads = self.cores
+			threads = self.maxCores
 		}
-		if memGB > self.memGB {
+
+		// Sanity check and cap to self.maxMemGB.
+		if memGB < 1 {
+			memGB = 1
+		}
+		if memGB > self.maxMemGB {
 			if log {
 				LogInfo("schedlr", "Need %d GB but settling for %d.", memGB,
-					self.memGB)
+					self.maxMemGB)
 			}
-			memGB = self.memGB
+			memGB = self.maxMemGB
 		}
 
 		// Acquire cores.
@@ -104,8 +124,8 @@ func (self *Scheduler) Enqueue(cmd *exec.Cmd, threads int, memGB int,
 		}
 		self.coreSem.P(threads)
 		if log {
-			LogInfo("schedlr", "Acquiring %d core%s (%d in use).", threads,
-				pluralize(threads), len(self.coreSem))
+			LogInfo("schedlr", "Acquiring %d core%s (%d/%d in use).", threads,
+				pluralize(threads), len(self.coreSem), self.maxCores)
 		}
 
 		// Acquire memory.
@@ -114,8 +134,8 @@ func (self *Scheduler) Enqueue(cmd *exec.Cmd, threads int, memGB int,
 		}
 		self.memGBSem.P(memGB)
 		if log {
-			LogInfo("schedlr", "Acquiring %d GB (%d in use).", memGB,
-				len(self.memGBSem))
+			LogInfo("schedlr", "Acquiring %d GB (%d/%d in use).", memGB,
+				len(self.memGBSem), self.maxMemGB)
 		}
 
 		cmd.Start()
@@ -123,21 +143,21 @@ func (self *Scheduler) Enqueue(cmd *exec.Cmd, threads int, memGB int,
 
 		self.coreSem.V(threads)
 		if log {
-			LogInfo("schedlr", "Releasing %d core%s (%d in use).", threads,
-				pluralize(threads), len(self.coreSem))
+			LogInfo("schedlr", "Releasing %d core%s (%d/%d in use).", threads,
+				pluralize(threads), len(self.coreSem), self.maxCores)
 		}
 		self.memGBSem.V(memGB)
 		if log {
-			LogInfo("schedlr", "Releasing %d GB (%d in use).", memGB,
-				len(self.memGBSem))
+			LogInfo("schedlr", "Releasing %d GB (%d/%d in use).", memGB,
+				len(self.memGBSem), self.maxMemGB)
 		}
 	}()
 }
 
-func (self *Scheduler) getCores() int {
-	return self.cores
+func (self *Scheduler) getMaxCores() int {
+	return self.maxCores
 }
 
-func (self *Scheduler) getMemGB() int {
-	return self.memGB
+func (self *Scheduler) getMaxMemGB() int {
+	return self.maxMemGB
 }
