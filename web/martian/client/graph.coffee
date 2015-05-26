60f08e9e99987d15bcd8ab4d4e8658cc617804c0
Ryan Wilson <ryan@10xtechnologies.com>
2015-05-25 17:21:16 -0700

    Disable annoying pop-up when Martian UI gets error for http requests

--- web/martian/client/graph.coffee
+++ web/martian/client/graph.coffee
@@ -229,6 +229,9 @@ app.controller('MartianGraphCtrl', ($scope, $compile, $http, $interval) ->
             , 3000)
         ).error(() ->
             $scope.showRestart = true
+            console.log('Server responded with an error for /api/restart, so stopping auto-refresh.')
+            $interval.cancel($scope.stopRefresh)
+            alert('mrp is no longer running.\n\nPlease run mrp again with the --noexit option to continue running the pipeline.')
         )
 
     $scope.expandString = (view, index, name) ->
@@ -256,7 +259,5 @@ app.controller('MartianGraphCtrl', ($scope, $compile, $http, $interval) ->
         ).error(() ->
             console.log('Server responded with an error for /api/get-state, so stopping auto-refresh.')
             $interval.cancel($scope.stopRefresh)
-            $scope.stopRefresh = null
-            alert('mrp is no longer running.\n\nPlease run mrp again to continue running the pipeline.')
         )
 )
--- web/martian/client/graph.js
+++ web/martian/client/graph.js
@@ -348,7 +348,10 @@
           return $scope.refresh();
         }, 3000);
       }).error(function() {
-        return $scope.showRestart = true;
+        $scope.showRestart = true;
+        console.log('Server responded with an error for /api/restart, so stopping auto-refresh.');
+        $interval.cancel($scope.stopRefresh);
+        return alert('mrp is no longer running.\n\nPlease run mrp again with the --noexit option to continue running the pipeline.');
       });
     };
     $scope.expandString = function(view, index, name) {
@@ -386,9 +389,7 @@
         return $scope.showRestart = true;
       }).error(function() {
         console.log('Server responded with an error for /api/get-state, so stopping auto-refresh.');
-        $interval.cancel($scope.stopRefresh);
-        $scope.stopRefresh = null;
-        return alert('mrp is no longer running.\n\nPlease run mrp again to continue running the pipeline.');
+        return $interval.cancel($scope.stopRefresh);
       });
     };
   });
