cdbd7837f98ea059a3e912ad9f0894109b04ac26
David Stafford <dstaff@10xgenomics.com>
2016-05-18 15:29:38 -0700

    Use MRO path

--- src/martian/mrt/main.go
+++ src/martian/mrt/main.go
@@ -3,15 +3,13 @@ package main
 import (
 	"flag"
 	"martian/core"
+	"os"
 	"strings"
 )
 
-var input_new_mro = flag.String("mro", "", "Invocation for new pipestance")
+var input_new_mro = flag.String("mro", "", "MRO invocation file for new pipestance")
 var input_new_psid = flag.String("psid", "", "Pipestance ID for new pipestance")
-var new_mro_paths = flag.String("mropath", "./", "Search string for MROs")
-
 var old_pipestance_path = flag.String("base", "", "Base pipestance path")
-
 var invalidated_stages = flag.String("inv", "", "Stages i changed")
 
 func main() {
@@ -22,9 +20,7 @@ func main() {
 	oldi.Srcpath = *old_pipestance_path + "/_mrosource"
 	oldi.Psid = "x"
 	oldi.PipestancePath = *old_pipestance_path
-	//oldi.MroPaths = []string{}
-	// This makes it work but is like totally wrong n stuff *** XXX YYY ZZZ XXX YYY ZZZ
-	oldi.MroPaths = core.ParseMroPath(*new_mro_paths)
+	oldi.MroPaths = core.ParseMroPath(*old_pipestance_path)
 	oldi.MroVersion = "x"
 	oldi.Envs = map[string]string{}
 
@@ -34,7 +30,7 @@ func main() {
 	newi.PipestancePath = *input_new_psid
 	newi.Psid = *input_new_psid
 	//newi.MroPaths= strings.Split(*new_mro_paths, ":");
-	newi.MroPaths = core.ParseMroPath(*new_mro_paths)
+	newi.MroPaths = core.ParseMroPath(os.Getenv("MROPATH"))
 	newi.MroVersion = "y"
 	newi.Envs = map[string]string{}
 
