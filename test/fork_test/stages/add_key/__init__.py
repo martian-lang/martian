658f64c2a40d60dbb40c5651a9c35e5f407d0da5
Adam Azarchs <adam.azarchs@10xgenomics.com>
2016-09-22 17:50:14 -0700

    Add a mechanism to the test pipeline to force failure.

--- test/fork_test/stages/add_key/__init__.py
+++ test/fork_test/stages/add_key/__init__.py
@@ -1,5 +1,6 @@
 import martian
 import json
+import os
 import random
 
 __MRO__ = """
@@ -7,12 +8,21 @@ stage ADD_KEY(
     in string key,
     in string value,
     in json start,
+    in string failfile,
     out json result,
     src py "stages/add_key",
 )
 """
 
 def main(args, outs):
+  failfile = os.path.abspath(os.path.join(
+      os.path.dirname(__file__), '..', '..', args.failfile))
+  if args.failfile and os.path.isfile(failfile):
+    s = ''
+    with open(failfile, 'r') as f:
+      s = f.read()
+    os.unlink(failfile)
+    os.kill(os.getpid(), int(s))
   if args.start:
     with open(args.start, 'r') as inpf:
       s = json.load(inpf)
