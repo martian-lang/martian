5e6fe136ad7659298418e7f0a7782b075f922228
Adam Azarchs <adam.azarchs@10xgenomics.com>
2017-08-14 15:10:58 -0700

    Cleanup: Move GetCPUInfo into a separate file.
    It's platform-specific, and we don't even use it.

--- src/martian/core/jobmanager.go
+++ src/martian/core/jobmanager.go
@@ -6,18 +6,15 @@
 package core
 
 import (
-	"bufio"
 	"bytes"
 	"encoding/json"
 	"fmt"
 	"github.com/cloudfoundry/gosigar"
-	"io"
 	"io/ioutil"
 	"os"
 	"os/exec"
 	"path"
 	"runtime"
-	"strconv"
 	"strings"
 	"syscall"
 	"time"
@@ -25,67 +22,6 @@ import (
 
 const maxRetries = 5
 const retryExitCode = 513
-
-func GetCPUInfo() (int, int, int, int) {
-	// From Linux /proc/cpuinfo, count sockets, physical cores, and logical cores.
-	// runtime.numCPU() does not provide this
-	//
-	fname := "/proc/cpuinfo"
-
-	f, err := os.Open(fname)
-	if err != nil {
-		return 0, 0, 0, 0
-	}
-	defer f.Close()
-
-	reader := bufio.NewReader(f)
-	sockets := -1
-	physicalCoresPerSocket := -1
-	logicalCores := -1
-
-	for {
-		line, _, err := reader.ReadLine()
-		if err == io.EOF {
-			break
-		}
-
-		fields := strings.Split(string(line), ":")
-		if len(fields) < 2 {
-			continue
-		}
-
-		k := strings.TrimSpace(fields[0])
-		v := strings.TrimSpace(fields[1])
-
-		switch k {
-		case "physical id":
-			i, err := strconv.ParseInt(v, 10, 32)
-			if err == nil && int(i) > sockets {
-				sockets = int(i)
-			}
-		case "core id":
-			i, err := strconv.ParseInt(v, 10, 32)
-			if err == nil && int(i) > physicalCoresPerSocket {
-				physicalCoresPerSocket = int(i)
-			}
-		case "processor":
-			i, err := strconv.ParseInt(v, 10, 32)
-			if err == nil && int(i) > logicalCores {
-				logicalCores = int(i)
-			}
-		}
-	}
-
-	if sockets > -1 && physicalCoresPerSocket > -1 && logicalCores > -1 {
-		sockets += 1
-		physicalCoresPerSocket += 1
-		physicalCores := sockets * physicalCoresPerSocket
-		logicalCores += 1
-		return sockets, physicalCoresPerSocket, physicalCores, logicalCores
-	} else {
-		return 0, 0, 0, 0
-	}
-}
 
 //
 // Job managers
