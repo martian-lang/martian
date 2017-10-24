martian-lang/martian

commit 082d509c4fc80af0b3aec0d2bd072ff5909b6386

8-bit encode all strings coming from stage code.

adapters/python/martian_shell.py
@@ -239,26 +239,28 @@ class _Metadata(object):
     def log(self, level, message):
         """Write a log line to the log file."""
         self._logfile.write('%s [%s] %s\n' %
-                            (self.make_timestamp_now(), level, message))
+                            (self.make_timestamp_now(),
+                             level,
+                             message.encode('utf-8')))
         self._logfile.flush()
 
     def alarm(self, message):
         """Append to the alarms file."""
-        self._append(message, 'alarm')
+        self._append(message.encode('utf-8'), 'alarm')
 
     def progress(self, message):
         """Report a progress update."""
-        self.write_raw_atomic('progress', message, True)
+        self.write_raw_atomic('progress', message.encode('utf-8'), True)
 
     @staticmethod
     def write_errors(message):
         """Write to the errors file."""
         with os.fdopen(4, 'w') as error_out:
-            error_out.write(message)
+            error_out.write(message.encode('utf-8'))
 
     def write_assert(self, message):
         """Write to the assert file."""
-        self.write_errors('ASSERT:' + message)
+        self.write_errors('ASSERT:' + message.encode('utf-8'))
 
     def update_journal(self, name, force=False):
         """Write a journal entry notifying the parent mrp process of changes to

test/fork_test/expected_fail1/AWESOME/ADD_KEY1/fork0/chnk0/_errors
@@ -1,11 +1,11 @@
 Traceback (most recent call last):
-  File "/Users/testuser/martian/adapters/python/martian_shell.py", line 524, in _main
+  File "/Users/testuser/martian/adapters/python/martian_shell.py", line 526, in _main
     stage.main()
-  File "/Users/testuser/martian/adapters/python/martian_shell.py", line 490, in main
+  File "/Users/testuser/martian/adapters/python/martian_shell.py", line 492, in main
     self._run(lambda: self._module.main(args, outs))
-  File "/Users/testuser/martian/adapters/python/martian_shell.py", line 459, in _run
+  File "/Users/testuser/martian/adapters/python/martian_shell.py", line 461, in _run
     cmd()
-  File "/Users/testuser/martian/adapters/python/martian_shell.py", line 490, in <lambda>
+  File "/Users/testuser/martian/adapters/python/martian_shell.py", line 492, in <lambda>
     self._run(lambda: self._module.main(args, outs))
   File "/Users/testuser/martian/test/fork_test/stages/add_key/__init__.py", line 25, in main
     os.kill(os.getpid(), int(s))

test/split_test/expected/SUM_SQUARE_PIPELINE/REPORT/fork0/chnk0/_log
@@ -1,2 +1,3 @@
-2017-07-12 17:30:19 [time] __start__
-2017-07-12 17:30:19 [time] __end__
+2017-10-16 15:36:38 [time] __start__
+2017-10-16 15:36:38 [info] Logging à non-ascii character.
+2017-10-16 15:36:38 [time] __end__

test/split_test/stages/report/__init__.py
@@ -1,3 +1,5 @@
+# encoding: utf-8
+
 import martian
 
 __MRO__ = """
@@ -8,6 +10,7 @@ stage REPORT(
 """
 
 def main(args, outs):
-    martian.update_progress('%s = %f' % (
-        '+'.join(['%f^2' % v for v in args.values]),
+    martian.log_info(u'Logging à non-ascii character.')
+    martian.update_progress(u'%s = %f' % (
+        u'+'.join([u'%f\u00b2' % v for v in args.values]),
         args.sum))

test/split_test/expected/SUM_SQUARE_PIPELINE/REPORT/fork0/chnk0/_progress
@@ -1,1 +1,1 @@
-1.000000^2+2.000000^2+3.000000^2 = 14.000000
+1.000000²+2.000000²+3.000000² = 14.000000

commit 7126d3ddaf975210a2eb7b446d970d138f85d9f5

Convert objects to strings before encoding in log.
Fixes regression caused by 082d509c4fc80af0b3aec0d2bd072ff5909b6386

adapters/python/martian_shell.py
@@ -238,6 +238,9 @@ class _Metadata(object):
 
     def log(self, level, message):
         """Write a log line to the log file."""
+        if not isinstance(message, basestring):
+            # If not a basestring (str or unicode), convert to string here
+            message = str(message)
         self._logfile.write('%s [%s] %s\n' %
                             (self.make_timestamp_now(),
                              level,

test/fork_test/expected_fail1/AWESOME/ADD_KEY1/fork0/chnk0/_errors
@@ -1,11 +1,11 @@
 Traceback (most recent call last):
-  File "/Users/testuser/martian/adapters/python/martian_shell.py", line 526, in _main
+  File "/Users/testuser/martian/adapters/python/martian_shell.py", line 529, in _main
     stage.main()
-  File "/Users/testuser/martian/adapters/python/martian_shell.py", line 492, in main
+  File "/Users/testuser/martian/adapters/python/martian_shell.py", line 495, in main
     self._run(lambda: self._module.main(args, outs))
-  File "/Users/testuser/martian/adapters/python/martian_shell.py", line 461, in _run
+  File "/Users/testuser/martian/adapters/python/martian_shell.py", line 464, in _run
     cmd()
-  File "/Users/testuser/martian/adapters/python/martian_shell.py", line 492, in <lambda>
+  File "/Users/testuser/martian/adapters/python/martian_shell.py", line 495, in <lambda>
     self._run(lambda: self._module.main(args, outs))
   File "/Users/testuser/martian/test/fork_test/stages/add_key/__init__.py", line 25, in main
     os.kill(os.getpid(), int(s))

test/split_test/expected/SUM_SQUARE_PIPELINE/REPORT/fork0/chnk0/_log
@@ -1,3 +1,4 @@
-2017-10-16 15:36:38 [time] __start__
-2017-10-16 15:36:38 [info] Logging à non-ascii character.
-2017-10-16 15:36:38 [time] __end__
+2017-10-23 10:28:48 [time] __start__
+2017-10-23 10:28:48 [info] Logging à non-ascii character.
+2017-10-23 10:28:48 [info] {'hello': 'world'}
+2017-10-23 10:28:48 [time] __end__

test/split_test/stages/report/__init__.py
@@ -11,6 +11,7 @@ stage REPORT(
 
 def main(args, outs):
     martian.log_info(u'Logging à non-ascii character.')
+    martian.log_info({'hello': 'world'})
     martian.update_progress(u'%s = %f' % (
         u'+'.join([u'%f\u00b2' % v for v in args.values]),
         args.sum))
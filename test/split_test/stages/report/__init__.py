7126d3ddaf975210a2eb7b446d970d138f85d9f5
Adam Azarchs <adam.azarchs@10xgenomics.com>
2017-10-23 10:43:26 -0700

    Convert objects to strings before encoding in log.
    Fixes regression caused by 082d509c4fc80af0b3aec0d2bd072ff5909b6386

--- test/split_test/stages/report/__init__.py
+++ test/split_test/stages/report/__init__.py
@@ -11,6 +11,7 @@ stage REPORT(
 
 def main(args, outs):
     martian.log_info(u'Logging à non-ascii character.')
+    martian.log_info({'hello': 'world'})
     martian.update_progress(u'%s = %f' % (
         u'+'.join([u'%f\u00b2' % v for v in args.values]),
         args.sum))
