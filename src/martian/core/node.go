martian-lang/martian

commit 9a187c2d83bf6c02458ad64ec179778969799158

Allow binding from array elements.
* Enforce type match for all elements of arrays.

Previously, it was only enforced that the first element of the array was
of the correct type.

This is a compile-time check.  MRC passes for longranger, cellranger,
and cellranger-dna.

* Add constants for the various Kind types for Expression objects.

* Allow binding logic to recurse into arrays.

This allows bindings such as for example

call FOO (
    argv = [
        STAGE1.out,
        STAGE2.out,
    ],
)

* Allow trailing comma in array or map expressions.

This allows python-style lists, such as
[
   1,
   2,
   3,
],
to be used.

To maintain compatibility with older mrp versions, this final comma is
omitted by mrf, for now.  We'll add it back later, maybe, once we're
confident everyone is on an up to date mrp.

src/martian/core/node.go
@@ -135,27 +135,52 @@ func NewNode(parent Nodable, kind string, callStm *syntax.CallStm, callables *sy
 		self.argbindings[id] = binding
 		self.argbindingList = append(self.argbindingList, binding)
 	}
-	for _, binding := range self.argbindingList {
-		if binding.mode == "reference" && binding.boundNode != nil {
-			prenode := binding.boundNode
-			prename := prenode.getNode().fqname
-			// prenodes may get added later by setPrenode, however right now
-			// only prenodes induced directly by bindings are in the list.
-			// These are tracked separately for display in the UI.
-			if existing, ok := self.prenodes[prename]; !ok {
-				self.prenodes[prename] = prenode
-				self.directPrenodes = append(self.directPrenodes, binding.parentNode)
-			} else if existing != prenode {
-				util.LogInfo("runtime",
-					"WARNING: multiple prenodes with the same fqname %s for %s",
-					prename, self.fqname)
-			}
-			prenode.getNode().postnodes[self.fqname] = self
-		}
+	prenodes, directPrenodes := recurseBoundNodes(self.argbindingList)
+	for key, prenode := range prenodes {
+		self.prenodes[key] = prenode
+		prenode.getNode().postnodes[self.fqname] = self
+	}
+	for _, parent := range directPrenodes {
+		self.directPrenodes = append(self.directPrenodes, parent)
 	}
 	// Do not set state = getState here, or else nodes will wrongly report
 	// complete before the first refreshMetadata call
 	return self
+}
+
+// Get the set of distinct precurser nodes and direct precurser nodes based on
+// the given binding set.
+func recurseBoundNodes(bindingList []*Binding) (prenodes map[string]Nodable, parents []Nodable) {
+	found := make(map[string]Nodable)
+	allParents := make(map[Nodable]struct{})
+	parentList := make([]Nodable, 0, len(bindingList))
+	addPrenode := func(prenode Nodable) {
+		prename := prenode.getNode().fqname
+		if existing, ok := found[prename]; !ok {
+			found[prename] = prenode
+		} else if existing != prenode {
+			util.LogInfo("runtime",
+				"WARNING: multiple prenodes with the same fqname %s",
+				prename)
+		}
+	}
+	for _, binding := range bindingList {
+		if binding.mode == "reference" && binding.boundNode != nil {
+			addPrenode(binding.boundNode)
+		} else if binding.mode == "array" {
+			prenodes, parents := recurseBoundNodes(binding.value.([]*Binding))
+			for _, prenode := range prenodes {
+				addPrenode(prenode)
+			}
+			for _, parent := range parents {
+				if _, ok := allParents[parent]; !ok {
+					allParents[parent] = struct{}{}
+					parentList = append(parentList, parent)
+				}
+			}
+		}
+	}
+	return found, parentList
 }
 
 //

commit 4f40bb63675c256f1363d427fceeb689fe6a9441

Fix graph rendering bug.
Direct prenodes weren't being added correctly.  Those are only used to
compute edges in the UI graph (actual runtime uses indirect prenodes).

This fixes a regression caused by 9a187c2d83bf6c02458ad64ec179778969799158

src/martian/core/node.go
@@ -140,9 +140,7 @@ func NewNode(parent Nodable, kind string, callStm *syntax.CallStm, callables *sy
 		self.prenodes[key] = prenode
 		prenode.getNode().postnodes[self.fqname] = self
 	}
-	for _, parent := range directPrenodes {
-		self.directPrenodes = append(self.directPrenodes, parent)
-	}
+	self.directPrenodes = append(self.directPrenodes, directPrenodes...)
 	// Do not set state = getState here, or else nodes will wrongly report
 	// complete before the first refreshMetadata call
 	return self
@@ -167,6 +165,11 @@ func recurseBoundNodes(bindingList []*Binding) (prenodes map[string]Nodable, par
 	for _, binding := range bindingList {
 		if binding.mode == "reference" && binding.boundNode != nil {
 			addPrenode(binding.boundNode)
+			parent := binding.parentNode
+			if _, ok := allParents[parent]; !ok {
+				allParents[parent] = struct{}{}
+				parentList = append(parentList, parent)
+			}
 		} else if binding.mode == "array" {
 			prenodes, parents := recurseBoundNodes(binding.value.([]*Binding))
 			for _, prenode := range prenodes {