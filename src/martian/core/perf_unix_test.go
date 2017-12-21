509c38525cb87093d23435a3da4bf6576cbeb34b
Adam Azarchs <adam.azarchs@10xgenomics.com>
2017-12-20 16:28:02 -0800

    Check in basic test for perf_unix.

---
+++ src/martian/core/perf_unix_test.go
@@ -0,0 +1,19 @@
+//
+// Copyright (c) 201 10X Genomics, Inc. All rights reserved.
+//
+// Converts system rusage into our structures.
+//
+
+package core
+
+import "testing"
+
+func TestGetUserProcessCount(t *testing.T) {
+	if c, err := GetUserProcessCount(); err != nil {
+		t.Error(err)
+	} else if c < 2 {
+		t.Errorf("Expected at least 2 processes for this uid, got %d.", c)
+	} else {
+		t.Log("Current user process count", c)
+	}
+}
