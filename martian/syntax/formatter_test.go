07a159cd689efa1325b5fd4b0a0d412043238639
Adam Azarchs <adam.azarchs@10xgenomics.com>
2018-08-21 11:03:48 -0700

    Add a formatter test case for src with an arg.

--- martian/syntax/formatter_test.go
+++ martian/syntax/formatter_test.go
@@ -162,7 +162,7 @@ stage ADD_KEY3(
 stage ADD_KEY5(
     in  string   key,
     in  string[] value,
-    src py       "stages/whatever",
+    src py       "stages/whatever arg",
 )
 
 stage SUM_SQUARES(
