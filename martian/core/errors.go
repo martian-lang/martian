ec7c92d41fd69a85113f5c677fe0965248f85dd4
Adam Azarchs <adam.azarchs@10xgenomics.com>
2019-05-24 12:25:10 -0700

    Remove some obsolete error types.

--- martian/core/errors.go
+++ martian/core/errors.go
@@ -10,7 +10,8 @@ import (
 	"fmt"
 )
 
-// RuntimeError
+// RuntimeError is returned to indicate a failure to instantiate or
+// advance a pipeline at runtime.
 type RuntimeError struct {
 	Msg string
 }
@@ -19,105 +20,68 @@ func (self *RuntimeError) Error() string {
 	return fmt.Sprintf("RuntimeError: %s.", self.Msg)
 }
 
-// PipestanceInvocationError
+// PipestanceInvocationError is returned when attempting to reattach to a
+// pipestance directory if the mro source code has changed.
 type PipestanceInvocationError struct {
 	Psid           string
 	InvocationPath string
 }
 
 func (self *PipestanceInvocationError) Error() string {
-	return fmt.Sprintf("RuntimeError: pipestance '%s' already exists with different invocation file %s",
+	return fmt.Sprintf(
+		"RuntimeError: pipestance '%s' already exists with different "+
+			"invocation file %s",
 		self.Psid, self.InvocationPath)
 }
 
-// PipestancePathError
+// PipestancePathError is returned when attempting to reattach to a pipestance
+// if the target directory does not appear to be a pipestance.
 type PipestancePathError struct {
 	Path string
 }
 
 func (self *PipestancePathError) Error() string {
-	return fmt.Sprintf("RuntimeError: %s is not a pipestance directory", self.Path)
+	return fmt.Sprintf(
+		"RuntimeError: %s is not a pipestance directory",
+		self.Path)
 }
 
-// PipestanceJobModeError
+// PipestanceJobModeError is returned when attempting to reattach to a
+// pipestance which was started with a different job mode.
 type PipestanceJobModeError struct {
 	Psid    string
 	JobMode string
 }
 
 func (self *PipestanceJobModeError) Error() string {
-	return fmt.Sprintf("RuntimeError: pipestance '%s' was originally started in job mode '%s'. Please try running again in job mode '%s'.", self.Psid, self.JobMode, self.JobMode)
+	return fmt.Sprintf(
+		"RuntimeError: pipestance '%s' was originally started "+
+			"in job mode '%s'. Please try running again in job mode '%s'.",
+		self.Psid, self.JobMode, self.JobMode)
 }
 
-// PipestanceLockedError
+// PipestanceLockedError is returned when attempting to Lock a
+// pipestance which is already locked.
 type PipestanceLockedError struct {
 	Psid           string
 	PipestancePath string
 }
 
 func (self *PipestanceLockedError) Error() string {
-	return fmt.Sprintf("RuntimeError: pipestance '%s' already exists and is locked by another Martian instance. If you are sure no other Martian instance is running, delete the _lock file in %s and start Martian again.", self.Psid, self.PipestancePath)
+	return fmt.Sprintf(
+		"RuntimeError: pipestance '%s' already exists and is locked by "+
+			"another Martian instance. If you are sure no other Martian "+
+			"instance is running, delete the _lock file in %s and start "+
+			"Martian again.",
+		self.Psid, self.PipestancePath)
 }
 
-// PipestanceNotFailedError
-type PipestanceNotFailedError struct {
-	Psid string
-}
-
-func (self *PipestanceNotFailedError) Error() string {
-	return fmt.Sprintf("RuntimeError: pipestance '%s' is not failed.", self.Psid)
-}
-
-// PipestanceNotRunningError
-type PipestanceNotRunningError struct {
-	Psid string
-}
-
-func (self *PipestanceNotRunningError) Error() string {
-	return fmt.Sprintf("RuntimeError: pipestance '%s' is not running.", self.Psid)
-}
-
-// PipestanceNotExistsError
-type PipestanceNotExistsError struct {
-	Psid string
-}
-
-func (self *PipestanceNotExistsError) Error() string {
-	return fmt.Sprintf("RuntimeError: pipestance '%s' doesn't exist.", self.Psid)
-}
-
-// PipestanceExistsError
+// PipestanceExistsError is returned by the runtime when attempting to instantiate
+// a pipestance in a directory which already has a pipestance.
 type PipestanceExistsError struct {
 	Psid string
 }
 
 func (self *PipestanceExistsError) Error() string {
 	return fmt.Sprintf("RuntimeError: pipestance '%s' already exists.", self.Psid)
-}
-
-// PipestanceSizeError
-type PipestanceSizeError struct {
-	Psid string
-}
-
-func (self *PipestanceSizeError) Error() string {
-	return fmt.Sprintf("RuntimeError: Cannot compute size requirement for %s.", self.Psid)
-}
-
-// PipestanceCopyingError
-type PipestanceCopyingError struct {
-	Psid string
-}
-
-func (self *PipestanceCopyingError) Error() string {
-	return fmt.Sprintf("RuntimeError: pipestance '%s' is currently being copied.", self.Psid)
-}
-
-// PipestanceWipeError
-type PipestanceWipeError struct {
-	Psid string
-}
-
-func (self *PipestanceWipeError) Error() string {
-	return fmt.Sprintf("RuntimeError: pipestance '%s' cannot be wiped.", self.Psid)
 }
