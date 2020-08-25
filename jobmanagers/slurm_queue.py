2e05e91307d71cae2b687f71aaeecc4c725c8d7a
Adam Azarchs <adam.azarchs@10xgenomics.com>
2020-08-25 14:20:10 -0700

    Add a queue-check script for slurm.
    This allows mrp with --jobmode=slurm to detect when jobs fail without
    writing any files, e.g. if the cluster manager killed them before they
    began.

---
+++ jobmanagers/slurm_queue.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2020 10x Genomics, Inc. All rights reserved.
+#
+
+"""Queries squeue about a list of jobs and parses the output, returning the list
+of jobs which are queued, running, or on hold."""
+
+import subprocess
+import sys
+
+
+def get_ids():
+    """Returns he set of jobids to query from standard input."""
+    ids = []
+    for jobid in sys.stdin.readlines():
+        ids.append(jobid.strip())
+    return ids
+
+
+def mkopts(ids):
+    """Gets the command line for qstat."""
+    if not ids:
+        sys.exit(0)
+    return ["squeue", "-o", r"%A %t", "-j", ",".join(ids)]
+
+
+def execute(cmd):
+    """Executes qstat and captures its output."""
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = proc.communicate()
+    if proc.returncode:
+        raise OSError(err)
+    if len(out) < 500:
+        sys.stderr.write(out)
+    else:
+        sys.stderr.write(out[:496] + "...")
+    return out
+
+
+def allow_state(state):
+    """Returns True if the state code is for a queued or running job."""
+    return state in ["CG", "PD", "R", "RD", "RS", "SO"]
+
+
+def parse_output(out):
+    """Parses the output of squeue and yields the ids of pending
+    jobs."""
+    for line in out.split("\n"):
+        if not line:
+            continue
+        line = line.strip()
+        if not line:
+            continue
+        line = line.split(None, 1)
+        if len(line) == 2 and allow_state(line[1]):
+            yield line[0]
+
+
+def main():
+    """Reads a set of ids from standard input, queries squeue, and outputs the
+    jobids to standard output for jobs which are in the pending state."""
+    for jobid in parse_output(execute(mkopts(get_ids()))):
+        sys.stdout.write("%s\n" % jobid)
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
