1adab3c2973a7a916eeed5dbb4f96b712253a487
Adam Azarchs <adam.azarchs@10xgenomics.com>
2021-01-05 15:59:09 -0800

    Fix relative paths for formatting.
    Also support `${workspaceFolder}` in `mropath`.

--- tools/syntax/vscode/src/mro.ts
+++ tools/syntax/vscode/src/mro.ts
@@ -1,4 +1,6 @@
 import * as child_process from "child_process";
+import * as path from "path";
+import * as fs from "fs";
 import * as process from "process";
 import * as vscode from "vscode";
 
@@ -15,41 +17,64 @@ export async function mroFormat(
     if (fileName) {
         args.push(fileName);
     }
-    return (await executeMro(fileContent, args, mropath)).stdout;
+    const workspacePath = vscode.workspace.getWorkspaceFolder(
+        vscode.Uri.file(fileName)).uri;
+    return (await executeMro(workspacePath, fileContent, args, mropath)).stdout;
 }
 
-function getDefaultMroExecutablePath(): string {
+async function getDefaultMroExecutablePath(
+    workspacePath: vscode.Uri): Promise<string> {
     // Try to retrieve the executable from VS Code's settings. If it's not set,
     // just use "mro" as the default and get it from the system PATH.
     const mroConfig = vscode.workspace.getConfiguration("martian-lang");
-    const mroExecutable = mroConfig.get<string>("mroExecutable");
+    let mroExecutable = mroConfig.get<string>("mroExecutable");
     if (mroExecutable.length === 0) {
         return "mro";
+    }
+    mroExecutable = mroExecutable.replace(
+        "${workspaceFolder}",
+        workspacePath.fsPath
+    )
+    if (!path.isAbsolute(mroExecutable)) {
+        try {
+            await fs.promises.access(mroExecutable, fs.constants.R_OK);
+        } catch {
+            mroExecutable = vscode.Uri.joinPath(
+                workspacePath, mroExecutable).fsPath;
+        }
     }
     return mroExecutable;
 }
 
-function getMroEnv(mropath: string): any {
+function getMroEnv(mropath: string, workspacePath: vscode.Uri): any {
     if (mropath === "") {
         return process.env;
     }
     const env = { ...process.env };
+    mropath = mropath.replace(
+        "${workspaceFolder}",
+        workspacePath.fsPath
+    );
+    if (!path.isAbsolute(mropath)) {
+        mropath = vscode.Uri.joinPath(workspacePath, mropath).fsPath;
+    }
     env.MROPATH = mropath;
     return env;
 }
 
 function executeMro(
+    workspacePath: vscode.Uri,
     fileContent: string,
     args: string[],
     mropath: string,
 ): Promise<{ stdout: string; stderr: string }> {
-    return new Promise((resolve, reject) => {
+    return new Promise(async (resolve, reject) => {
         const execOptions = {
-            env: getMroEnv(mropath),
+            env: getMroEnv(mropath, workspacePath),
             maxBuffer: Number.MAX_SAFE_INTEGER,
         };
         const proc = child_process.execFile(
-            getDefaultMroExecutablePath(),
+            await getDefaultMroExecutablePath(workspacePath),
             args,
             execOptions,
             (error: Error, stdout: string, stderr: string) => {
