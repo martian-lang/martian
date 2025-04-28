83d8541c4f4486a6edc004f5673629215ab8196a
John Reeves <j.reeves@garvan.org.au>
2025-04-28 13:35:22 +1000

    Apply pyformat

--- jobmanagers/pbs_queue.py
+++ jobmanagers/pbs_queue.py
@@ -11,7 +11,8 @@ import sys
 import json
 
 # PBS Pro "job states" to be regarded as "alive"
-ALIVE = {"Q","H","W","S","R", "E"}
+ALIVE = {'Q', 'H', 'W', 'S', 'R', 'E'}
+
 
 def get_ids():
     """Returns the set of jobids to query from standard input."""
@@ -25,7 +26,7 @@ def mkopts(ids):
     """Gets the command line for qstat."""
     if not ids:
         sys.exit(0)
-    return ["qstat", "-x", "-F", "json", "-f"] + ids
+    return ['qstat', '-x', '-F', 'json', '-f'] + ids
 
 
 def execute(cmd):
@@ -39,24 +40,26 @@ def execute(cmd):
         if len(out) < 500:
             sys.stderr.write(out)
         else:
-            sys.stderr.write(out[:496] + "...")
+            sys.stderr.write(out[:496] + '...')
         return out
+
 
 def parse_output(out):
     """Parses the JSON-format output of qstat and yields the ids of pending
     jobs."""
     data = json.loads(out)
-    for jid, info in data.get("Jobs", {}).items():
-        if info.get("job_state") in ALIVE:
+    for jid, info in data.get('Jobs', {}).items():
+        if info.get('job_state') in ALIVE:
             yield jid
+
 
 def main():
     """Reads a set of ids from standard input, queries qstat, and outputs the
     jobids to standard output for jobs which are in the pending state."""
     for jobid in parse_output(execute(mkopts(get_ids()))):
-        sys.stdout.write(f"{jobid}\n")
+        sys.stdout.write(f'{jobid}\n')
     return 0
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     sys.exit(main())
